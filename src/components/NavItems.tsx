ce0671341ca1884379966e24d56fe0a5e08741d8
Aayush Bharti <aayushbh2002@gmail.com>
2024-08-12 21:35:12 +0530

    updated home page, created navbar, added assets

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,10 @@
+"use client" //for interactivity(like states)
+import { useState } from "react"
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | Number>(null)
+  return(
+    <div></div>
+  )
+}
+export default NavItems
